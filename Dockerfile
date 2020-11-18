229fc870a21b3ab44e918dd981379b403733ab6c
krysalead <krysalead@gmail.com>
2020-11-18 22:01:52 +0100

    build improvment

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM node:14.10-alpine
+ARG ENV 
+ARG PORTS
+LABEL maintainer="krysalead@gmail.com"
+EXPOSE $PORTS
 # install git, python and compiler for native dependency code
-RUN apk add git=2.29.2-r0 g++=9.3.0-r2 make=4.3-r0 python3=3.8.5-r0 --no-cache \
-    && ln -sf python3 /usr/bin/python \
-    && rm -R /var/cache/apk/*
+RUN apk add git=2.24.3-r0 g++=9.3.0-r0 make=4.2.1-r2 python3=3.8.2-r1 --no-cache
+RUN ln -sf python3 /usr/bin/python
+RUN rm -R /var/cache/apk/
 WORKDIR /usr/app
-# For dev we add nodemon, run first as the below once are redownloaded each build
-RUN npm install -g nodemon@2.0.6
-# For dev we add the typescript compiler
-RUN npm install -g typescript@4.0.5
+# For dev we add nodemon,typescript compiler, run first as the below ADD are redownloaded each build
+RUN if [ "$ENV" = "dev" ] ; then npm install -g nodemon@2.0.6 typescript@4.0.5; fi
 # install wait to allow starting after another process is available
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
 RUN chmod +x /wait
--- build.sh
+++ build.sh
@@ -1,19 +1,44 @@
 #!/bin/sh
 NODE_VERSION=14.10-alpine
 USERNAME=krysalead
+declare ports
+ports[dev]="4000 5858"
+ports[prd]="4000"
+declare suffix
+ports[dev]="-dev"
+ports[prd]=""
 
-# docker login --username=${USERNAME}
-
+DOCKERFILE="Dockerfile"
+echo "Validation '$DOCKERFILE'"
+# Lint the image
+docker run --rm -i -v $PWD/.hadolint.yaml:/root/.config/hadolint.yaml hadolint/hadolint < $DOCKERFILE
+if [ $? != 0 ];
+then
+    echo "Failed to lint the image, see error above"
+    exit 2
+fi
+# Authentication to the docker hub, it will interactively request your password
+docker login --username=${USERNAME} --password=${DOCKER_HUB_PASSWORD}
+if [ $? != 0 ];
+then
+    echo "Failed to login to the docker hub, see error above"
+    exit 1
+fi
 for IMAGE_TYPE in dev prd 
 do
-    IMAGE_NAME="knode-${IMAGE_TYPE}:${NODE_VERSION}"
-    DOCKERFILE="Dockerfile-${IMAGE_TYPE}"
-    echo "Validation $DOCKERFILE"
-    docker run --rm -i -v $PWD/.hadolint.yaml:/root/.config/hadolint.yaml hadolint/hadolint < $DOCKERFILE
+    IMAGE_NAME="dknode${suffix[$IMAGE_TYPE]}:${NODE_VERSION}"
     echo "Building $IMAGE_NAME"
-    # OUTPUT=$(docker build -t ${IMAGE_NAME} -f $DOCKERFILE . | grep "Successfully built")
-    # stringarray=($OUTPUT)
-    # echo ${stringarray[${#stringarray[@]} - 1]]}
-    # docker tag ${stringarray[${#stringarray[@]} - 1]]} ${USERNAME}/${IMAGE_NAME}
-    # docker push ${USERNAME}/${IMAGE_NAME}
+    # Get the hash of the image
+    OUTPUT=$(docker build -t ${IMAGE_NAME} -f $DOCKERFILE . --build-arg ENV=${IMAGE_TYPE} --build-arg PORTS=${ports[$IMAGE_TYPE]} | grep "Successfully built")
+    if [ $? != 0 ];
+    then
+        echo "Failed to build the image, see error above"
+        exit 3
+    fi
+    stringarray=($OUTPUT)
+    echo "Tagging $IMAGE_NAME"
+    # tag the image with the hash
+    docker tag ${stringarray[${#stringarray[@]} - 1]]} ${USERNAME}/${IMAGE_NAME}
+    echo "Pushing $IMAGE_NAME"
+    docker push ${USERNAME}/${IMAGE_NAME}
 done
